99f1cc268caa9a5c06ec41e27324c70a9aaf6d5c
Donghyuk Kang <donghyuk1003+github@gmail.com>
2022-11-12 19:17:14 +0900

    feat: configure root app module

---
+++ apps/rest-api/src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserEntity } from './modules/user/user.entity';
+import { UserModule } from './modules/user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      // envFilePath: !process.env.NODE_ENV
+      //   ? '.env.development'
+      //   : `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('MYSQL_HOST'),
+        port: config.get<number>('MYSQL_PORT'),
+        username: config.get('MYSQL_USER'),
+        password: config.get('MYSQL_PASSWORD'),
+        database: config.get('MYSQL_DATABASE'),
+        timezone: 'Z',
+        entities: [UserEntity],
+        /**
+         * disable on production
+         */
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UserModule,
+  ],
+})
+export class AppModule {}
--- apps/rest-api/src/main.ts
+++ apps/rest-api/src/main.ts
@@ -3,20 +3,16 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const config = app.get(ConfigService);
+  const port = config.get('PORT') || 3000;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
 }
 
 bootstrap();
