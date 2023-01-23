12753e75ea42a232918174dea3f4604d570c70b4
Donghyuk Kang <donghyuk1003+github@gmail.com>
2023-01-24 01:03:30 +0900

    fix: module import

--- apps/rest-api/src/modules/user/user.module.ts
+++ apps/rest-api/src/modules/user/user.module.ts
@@ -1,9 +1,10 @@
 import { UserDomainModule } from '@nest-react-native-monorepo/domain';
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from '../auth/auth.module';
 import { UserController } from './user.controller';
 @Module({
-  imports: [UserDomainModule],
+  imports: [UserDomainModule, AuthModule],
   controllers: [UserController],
 })
 export class UserModule {}
