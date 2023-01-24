b42e92eb5c715a965005827a869f7d6648d2ea7b
Donghyuk Kang <donghyuk1003+github@gmail.com>
2023-01-24 19:30:29 +0900

    test: mock react-native-code-push

--- apps/mobile-app/test-setup.ts
+++ apps/mobile-app/test-setup.ts
@@ -32,3 +32,25 @@ jest.mock('react-native-bootsplash', () => {
     getVisibilityStatus: jest.fn().mockResolvedValue('hidden'),
   };
 });
+
+jest.mock('react-native-code-push', () => {
+  const codepush = () => (app) => app;
+  Object.assign(codepush, {
+    InstallMode: {},
+    CheckFrequency: {},
+    SyncStatus: {},
+    UpdateState: {},
+    DeploymentStatus: {},
+    DEFAULT_UPDATE_DIALOG: {},
+    allowRestart: jest.fn(),
+    checkForUpdate: jest.fn(() => Promise.resolve(null)),
+    disallowRestart: jest.fn(),
+    getCurrentPackage: jest.fn(() => Promise.resolve(null)),
+    getUpdateMetadata: jest.fn(() => Promise.resolve(null)),
+    notifyAppReady: jest.fn(() => Promise.resolve()),
+    restartApp: jest.fn(),
+    sync: jest.fn(() => Promise.resolve(1)),
+    clearUpdates: jest.fn(),
+  });
+  return codepush;
+});
